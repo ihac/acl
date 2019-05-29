6c7aba29c2ad3813ad9562d3e1a8e0b5989b8f0c
Xiao An <hac@zju.edu.cn>
2019-05-29 10:11:02 +0800

    Support an almost complete list of DNS record types
    Signed-off-by: Xiao An <hac@zju.edu.cn>

--- firewall/setup.go
+++ firewall/setup.go
@@ -94,13 +94,85 @@ func setup(c *caddy.Controller) error {
 	return nil
 }
 
+// TODO: dns.Type == QType? (@ihac)
 func parseQype(raw string) (dns.Type, error) {
-	// TODO: add more qtype. (@ihac)
 	switch raw {
 	case "A":
 		return dns.Type(dns.TypeA), nil
 	case "AAAA":
 		return dns.Type(dns.TypeAAAA), nil
+	case "AFSDB":
+		return dns.Type(dns.TypeAFSDB), nil
+	case "CAA":
+		return dns.Type(dns.TypeCAA), nil
+	case "CDNSKEY":
+		return dns.Type(dns.TypeCDNSKEY), nil
+	case "CDS":
+		return dns.Type(dns.TypeCDS), nil
+	case "CERT":
+		return dns.Type(dns.TypeCERT), nil
+	case "CNAME":
+		return dns.Type(dns.TypeCNAME), nil
+	case "DHCID":
+		return dns.Type(dns.TypeDHCID), nil
+	case "DLV":
+		return dns.Type(dns.TypeDLV), nil
+	case "DNAME":
+		return dns.Type(dns.TypeDNAME), nil
+	case "DNSKEY":
+		return dns.Type(dns.TypeDNSKEY), nil
+	case "DS":
+		return dns.Type(dns.TypeDS), nil
+	case "HIP":
+		return dns.Type(dns.TypeHIP), nil
+	case "KEY":
+		return dns.Type(dns.TypeKEY), nil
+	case "KX":
+		return dns.Type(dns.TypeKX), nil
+	case "LOC":
+		return dns.Type(dns.TypeLOC), nil
+	case "MX":
+		return dns.Type(dns.TypeMX), nil
+	case "NAPTR":
+		return dns.Type(dns.TypeNAPTR), nil
+	case "NS":
+		return dns.Type(dns.TypeNS), nil
+	case "NSEC":
+		return dns.Type(dns.TypeNSEC), nil
+	case "NSEC3":
+		return dns.Type(dns.TypeNSEC3), nil
+	case "NSEC3PARAM":
+		return dns.Type(dns.TypeNSEC3PARAM), nil
+	case "OPENPGPKEY":
+		return dns.Type(dns.TypeOPENPGPKEY), nil
+	case "PTR":
+		return dns.Type(dns.TypePTR), nil
+	case "RRSIG":
+		return dns.Type(dns.TypeRRSIG), nil
+	case "RP":
+		return dns.Type(dns.TypeRP), nil
+	case "SIG":
+		return dns.Type(dns.TypeSIG), nil
+	case "SMIMEA":
+		return dns.Type(dns.TypeSMIMEA), nil
+	case "SOA":
+		return dns.Type(dns.TypeSOA), nil
+	case "SRV":
+		return dns.Type(dns.TypeSRV), nil
+	case "SSHFP":
+		return dns.Type(dns.TypeSSHFP), nil
+	case "TA":
+		return dns.Type(dns.TypeTA), nil
+	case "TKEY":
+		return dns.Type(dns.TypeTKEY), nil
+	case "TLSA":
+		return dns.Type(dns.TypeTLSA), nil
+	case "TSIG":
+		return dns.Type(dns.TypeTSIG), nil
+	case "TXT":
+		return dns.Type(dns.TypeTXT), nil
+	case "URI":
+		return dns.Type(dns.TypeURI), nil
 	default:
 		return 0, fmt.Errorf("Unexpected token '%s'; expect legal QTYPE", raw)
 	}
